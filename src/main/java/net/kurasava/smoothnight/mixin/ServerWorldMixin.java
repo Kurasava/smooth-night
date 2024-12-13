3a369ac05082c457ec5f178a2e01cefefc5a37b0
Kurasava <flashbladepoint@mail.ru>
2024-12-14 00:26:28 +1100

    Update to 1.21.3

--- src/main/java/net/kurasava/smoothnight/mixin/ServerWorldMixin.java
+++ src/main/java/net/kurasava/smoothnight/mixin/ServerWorldMixin.java
@@ -15,14 +15,18 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.SleepManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.profiler.Profilers;
 import net.minecraft.village.raid.RaidManager;
 import net.minecraft.world.EntityList;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import net.minecraft.world.tick.TickManager;
 import net.minecraft.world.tick.WorldTickScheduler;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.function.BooleanSupplier;
@@ -30,22 +34,55 @@ import java.util.function.BooleanSupplier;
 @Mixin(value = ServerWorld.class, priority = 999)
 public abstract class ServerWorldMixin {
 
+    @Unique
     private SmoothNight INSTANCE = SmoothNight.INSTANCE;
+
+    @Final
+    @Shadow
+    private SleepManager sleepManager;
+
+    @Shadow
+    private boolean inBlockTick;
+
+    @Final
+    @Shadow
+    List<ServerPlayerEntity> players;
+
+    @Final
+    @Shadow
+    private WorldTickScheduler<Block> blockTickScheduler;
+
+    @Final
+    @Shadow
+    private WorldTickScheduler<Fluid> fluidTickScheduler;
+
+    @Final
+    @Shadow
+    protected RaidManager raidManager;
+
+    @Shadow
+    private int idleTimeout;
+
+    @Shadow
+    private EnderDragonFight enderDragonFight;
+
+    @Final
+    @Shadow
+    EntityList entityList;
+
+    @Final
+    @Shadow
+    private ServerChunkManager chunkManager;
+
+    @Final
+    @Shadow
+    private ServerEntityManager<Entity> entityManager;
 
     @Shadow
     public abstract void setTimeOfDay(long timeOfDay);
 
     @Shadow
-    protected SleepManager sleepManager;
-
-    @Shadow
-    protected boolean inBlockTick;
-
-    @Shadow
-    protected List<ServerPlayerEntity> players;
-
-    @Shadow
-    protected abstract TickManager getTickManager();
+    public abstract TickManager getTickManager();
 
     @Shadow
     protected abstract void wakeSleepingPlayers();
@@ -57,57 +94,30 @@ public abstract class ServerWorldMixin {
     protected abstract void tickTime();
 
     @Shadow
-    public abstract void tickBlock(BlockPos pos, Block block);
+    protected abstract void tickBlock(BlockPos pos, Block block);
 
     @Shadow
-    public abstract void tickFluid(BlockPos pos, Fluid fluid);
+    protected abstract void tickFluid(BlockPos pos, Fluid fluid);
 
     @Shadow
-    protected WorldTickScheduler<Block> blockTickScheduler;
-
-    @Shadow
-    protected WorldTickScheduler<Fluid> fluidTickScheduler;
-
-    @Shadow
-    protected RaidManager raidManager;
-
-    @Shadow
-    protected abstract ServerChunkManager getChunkManager();
+    public abstract ServerChunkManager getChunkManager();
 
     @Shadow
     protected abstract void processSyncedBlockEvents();
 
     @Shadow
-    protected abstract LongSet getForcedChunks();
+    public abstract LongSet getForcedChunks();
 
     @Shadow
-    protected abstract void resetIdleTimeout();
+    public abstract void resetIdleTimeout();
 
     @Shadow
-    protected int idleTimeout;
-
-    @Shadow
-    protected EnderDragonFight enderDragonFight;
-
-    @Shadow
-    protected EntityList entityList;
-
-    @Shadow
-    protected abstract boolean shouldCancelSpawn(Entity entity);
-
-    @Shadow
-    protected ServerChunkManager chunkManager;
-
-    @Shadow
-    protected abstract void tickEntity(Entity entity);
-
-    @Shadow
-    protected ServerEntityManager<Entity> entityManager;
+    public abstract void tickEntity(Entity entity);
 
     @Overwrite
     public void tick(BooleanSupplier shouldKeepTicking) {
         ServerWorld world = (ServerWorld) (Object) this;
-        Profiler profiler = world.getProfiler();
+        Profiler profiler = Profilers.get();
         this.inBlockTick = true;
         TickManager tickManager = this.getTickManager();
         boolean bl = tickManager.shouldTick();
@@ -116,6 +126,7 @@ public abstract class ServerWorldMixin {
             world.getWorldBorder().tick();
             profiler.swap("weather");
             this.tickWeather();
+            profiler.pop();
         }
 
         int i = world.getGameRules().getInt(GameRules.PLAYERS_SLEEPING_PERCENTAGE);
@@ -128,7 +139,7 @@ public abstract class ServerWorldMixin {
             this.tickTime();
         }
 
-        profiler.swap("tickPending");
+        profiler.push("tickPending");
         if (!world.isDebugWorld() && bl) {
             long l = world.getTime();
             profiler.push("blockTicks");
@@ -167,13 +178,11 @@ public abstract class ServerWorldMixin {
 
             this.entityList.forEach((entity) -> {
                 if (!entity.isRemoved()) {
-                    if (this.shouldCancelSpawn(entity)) {
-                        entity.discard();
-                    } else if (!tickManager.shouldSkipTick(entity)) {
+                    if (!tickManager.shouldSkipTick(entity)) {
                         profiler.push("checkDespawn");
                         entity.checkDespawn();
                         profiler.pop();
-                        if (this.chunkManager.chunkLoadingManager.getTicketManager().shouldTickEntities(entity.getChunkPos().toLong())) {
+                        if (entity instanceof ServerPlayerEntity || this.chunkManager.chunkLoadingManager.getTicketManager().shouldTickEntities(entity.getChunkPos().toLong())) {
                             Entity entity2 = entity.getVehicle();
                             if (entity2 != null) {
                                 if (!entity2.isRemoved() && entity2.hasPassenger(entity)) {
@@ -191,8 +200,7 @@ public abstract class ServerWorldMixin {
                 }
             });
             profiler.pop();
-            WorldMixin world1 = (WorldMixin) (Object) this;
-            world1.tickBlockEntities();
+            ((WorldInvoker) world).tickBlockEntities();
         }
 
         profiler.push("entityManagement");
@@ -200,6 +208,8 @@ public abstract class ServerWorldMixin {
         profiler.pop();
     }
 
+
+    @Unique
     private void smoothNightSkip(ServerWorld world) {
         boolean doDayLightCycle = world.getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE);
 
@@ -224,9 +234,16 @@ public abstract class ServerWorldMixin {
         }
     }
 
+    @Unique
     private void sendTimeUpdatePacket(long time, long timeOfDay) {
         for (ServerPlayerEntity player : this.players) {
             player.networkHandler.sendPacket(new WorldTimeUpdateS2CPacket(time, timeOfDay, true));
         }
     }
+}
+
+@Mixin(World.class)
+interface WorldInvoker {
+    @Invoker("tickBlockEntities")
+    void tickBlockEntities();
 }
